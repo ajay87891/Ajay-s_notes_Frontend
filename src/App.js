83aaa27868ef1adbea41afc23cd1cece48ebe027
Ajay Singh <32979787+ajay87891@users.noreply.github.com>
2022-11-17 21:27:18 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './Components/About'
+import Home from './Components/Home'
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
    <>
-   
-   
+   <div className="bg-white/10">
+   <Router>
+        
+        <NavBar />
+        
+        
+        
+        <Routes>
+            <Route exact path="/about"
+              element = {<About />}
+            />
+            
+            <Route exact path="/" 
+            element={
+            <Home/>}
+            />
+            
+  
+          </Routes>
+          
+          </Router>
+          </div>
    </>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-4xl text-slate-800'>
+      This is Home
+    </div>
+  )
+}
+
+export default Home
+
